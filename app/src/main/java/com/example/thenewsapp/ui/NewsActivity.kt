8864d4ba85ca53f0d1cc5d504bd0dd902fbe93eb
ishan420 <kingerishan@gmail.com>
2024-06-25 13:17:04 +0530

    Adding XML layouts prerequisite

---
+++ app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.thenewsapp.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.thenewsapp.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
