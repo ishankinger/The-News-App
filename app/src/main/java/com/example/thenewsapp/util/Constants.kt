7daa113502747aa2629010927a5af3b137f73fe6
ishan420 <kingerishan@gmail.com>
2024-06-26 11:36:15 +0530

    Connecting to the API

---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "4875d61114d343d8aab62a3ca97dac70"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
