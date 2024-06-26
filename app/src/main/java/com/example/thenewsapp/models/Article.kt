e6c9b06f2ef01c23ef3be45db88030d357172581
ishan420 <kingerishan@gmail.com>
2024-06-26 12:13:58 +0530

    Adding a Room Database

--- app/src/main/java/com/example/thenewsapp/models/Article.kt
+++ app/src/main/java/com/example/thenewsapp/models/Article.kt
@@ -1,10 +1,15 @@
 package com.example.thenewsapp.models
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity(tableName = "articles")
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int? = null,
     val author : String,
     val content : String,
     val description : String,
